f4ce404d6e3dd967b929628e4107798fdc1d7541
Blake Zbikowski <blake@auroraeffect.io>
2025-02-15 23:33:50 -0800

    final code for tf portfolio project pre-cicd

--- bz-blog/terraform-js/state.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-    backend "s3" {
-        bucket = "bz-terraform-portfolio-project"
-        key = "global/s3/terraform.tfstate"
-        region = "us-west-1"
-        dynamodb_table = "terraform-lock-file"
-    }
-}
